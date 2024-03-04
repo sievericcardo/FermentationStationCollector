3536af41c68bc9b4c24a3d9e02e004e891ae97d9
Riccardo Sieve <sievericcardo@gmail.com>
2024-03-04 10:08:57 +0100

    Update asset.py
    added reading with serial reader

--- collector/assets/asset.py
+++ collector/assets/asset.py
@@ -8,11 +8,16 @@ from abc import ABC, abstractmethod
 from influxdb_client import Point
 
 from collector.influx.influx_controller import InfluxController
+from collector.sensors.read_serial import SerialReader
+from collector.assets.measurement import Measurement
 
 class Asset(ABC):
     """
     An asset that can be collected.
     """
+    def __init__(self, port, baudrate, timeout) -> None:
+        super().__init__()
+        self.serial_reader = SerialReader(port, baudrate, timeout)
 
     stop_flag: threading.Event = threading.Event()
     influx_controller: InfluxController = InfluxController()
@@ -37,14 +42,54 @@ class Asset(ABC):
         """
         pass
 
-    def collect(self) -> None:
+
+    def __process_data(self, data: str) -> None:
+        """
+        Process the data from the serial port.
+
+        Args:
+            data: The data from the serial port.
+        """
+        values = data.split(',')
+
+        if len(values) <= 2:
+            logging.error(f'Invalid data: {data}')
+            return
+        
+        if values[0] == 'TH':
+            id = values[1]
+            temperature = float(values[2])
+            humidity = float(values[3])
+
+            t = Point(Measurement.TEMPERATURE.get_measurement_name()) \
+                .tag('th_sensor', id) \
+                .field('value', temperature)
+            self.influx_controller.write(t)
+
+            h = Point(Measurement.HUMIDITY.get_measurement_name()) \
+                .tag('th_sensor', id) \
+                .field('value', humidity)
+            self.influx_controller.write(h)
+        elif values[0] == 'PH':
+            id = values[1]
+            ph = float(values[2])
+
+            p = Point(Measurement.PH.get_measurement_name()) \
+                .tag('ph_sensor', id) \
+                .field('value', ph)
+            self.influx_controller.write(p)
+
+    def __collect(self) -> None:
         """
         Collect the asset data.
         """
         while not self.stop_flag.is_set():
             try:
-                point = self.to_point()
-                self.influx_controller.write(point)
+                data = self.serial_reader.read()
+                self.__process_data(data)
+
+                # point = self.to_point()
+                # self.influx_controller.write(point)
                 time.sleep(self.sensor_read_interval)
             except Exception as e:
                 logging.error(f'Error collecting asset data: {e}')
@@ -56,5 +101,5 @@ class Asset(ABC):
         Start collecting the asset data.
         """
         self.stop_flag.clear()
-        thread = threading.Thread(target=self.collect)
+        thread = threading.Thread(target=self.__collect)
         thread.start()
