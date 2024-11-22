d870bac0d95d9ad605435020fc6999eb8788de61
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-22 16:53:24 +0100

    Added both config

--- collector/config.py
+++ collector/config.py
@@ -1,6 +1,9 @@
 import os
 
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yml")
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.ini")
+CONFIG_YML = os.path.join(os.path.dirname(__file__), "config.yml")
 
 if not os.path.isfile(CONFIG_PATH):
-    raise FileNotFoundError("Config file not found")
+    raise FileNotFoundError("Config ini file not found")
+if not os.path.isfile(CONFIG_YML):
+    raise FileNotFoundError("Config yml file not found")
